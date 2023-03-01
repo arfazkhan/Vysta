251f8a652e14fc2e38d7ae7189b2d56ab307b2ee
Khan <you@example.com>
2023-03-02 02:46:52 +0530

    index

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+#Index Page
 @app.route("/")
-def main():
-    return "Welcome!"
+def index():    
+    return render_template('index.html')    
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == "__main__":  
+    app.run(debug=True)     
